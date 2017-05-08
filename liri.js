e3b2b7a2cc2fa9c818ab79bd23ef73040827ff83
Ricardo Paulin <rpaulin@ricardos-mbp.attlocal.net>
2017-05-08 11:44:28 -0500

    added bonus of appending all the console logs and command line arguments in a text file

--- liri.js
+++ liri.js
@@ -30,11 +30,36 @@ var argument_name = argument_array.join(" ");
 
 var rotten_tomatoes_url = argument_array.join("_");
 
+var consoleLog = "";
+
+///// function to save content in a text file
+
+var saveIntoLog = function(){
+
+    var commandLine = process.argv[0] + " " + process.argv[1] + " " + argument + " " + argument_name + "\n";
+
+    fs.appendFile("log.txt", commandLine + consoleLog, function(err) {
+
+        // If an error was experienced we say it.
+        if (err) {
+            console.log(err);
+        }
+
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            console.log("Content Added!");
+        }
+
+    });
+
+};
+
 /////wrapping everything in a function
 
 var liri = function() {
 
 ///// MY-TWEETS LOGIC
+
 
     if (argument === "my-tweets") {
 
@@ -43,16 +68,20 @@ var liri = function() {
                 if (tweets.length < 20) {
                     for (var x = 0; x < tweets.length; x++) {
 
-                        console.log("Tweet created on " + tweets[x].created_at + " :" + tweets[x].text);
+                        console.log("Tweet created on " + tweets[x].created_at + ": " + tweets[x].text);
+                        consoleLog += "Tweet created on " + tweets[x].created_at + ": " + tweets[x].text + "\n";
 
                     }
+                    saveIntoLog();
                 }
                 else {
                     for (var i = 0; i < 20; i++) {
 
-                        console.log("Tweet created on " + tweets[i].created_at + " :" + tweets[i].text);
+                        console.log("Tweet created on " + tweets[i].created_at + ": " + tweets[i].text);
+                        consoleLog += "Tweet created on " + tweets[i].created_at + ": " + tweets[i].text + "\n";
 
                     }
+                    saveIntoLog();
                 }
             }
         });
@@ -75,7 +104,15 @@ var liri = function() {
                 console.log("Link to a preview of the track: " + data.tracks.items[3].preview_url);
                 console.log("Name of the album: " + data.tracks.items[3].album.name);
                 console.log("///////////////////////////////////////////////////")
-                // Do something with 'data'
+
+                consoleLog += "Artist Name: " + data.tracks.items[3].artists[0].name + "\n";
+                consoleLog += "Name of track: " + data.tracks.items[3].name + "\n";
+                consoleLog += "Link to a preview of the track: " + data.tracks.items[3].preview_url + "\n";
+                consoleLog += "Name of the album: " + data.tracks.items[3].album.name + "\n";
+                consoleLog += "///////////////////////////////////////////////////" + "\n";
+
+                saveIntoLog();
+
             });
         }
 
@@ -92,8 +129,16 @@ var liri = function() {
                     console.log("Name of track: " + data.tracks.items[z].name);
                     console.log("Link to a preview of the track: " + data.tracks.items[z].preview_url);
                     console.log("Name of the album: " + data.tracks.items[z].album.name);
-                    console.log("///////////////////////////////////////////////////")
+                    console.log("///////////////////////////////////////////////////");
+
+                    consoleLog += "Result " + (z + 1) + "\n";
+                    consoleLog += "Artist Name: " + data.tracks.items[z].artists[0].name + "\n";
+                    consoleLog += "Name of track: " + data.tracks.items[z].name + "\n";
+                    consoleLog += "Link to a preview of the track: " + data.tracks.items[z].preview_url + "\n";
+                    consoleLog += "Name of the album: " + data.tracks.items[z].album.name + "\n";
+                    consoleLog += "///////////////////////////////////////////////////" + "\n";
                 }
+                saveIntoLog();
             });
         }
     }
@@ -124,10 +169,21 @@ var liri = function() {
                 console.log("Actors in the movie: " + JSON.parse(body).Actors);
                 console.log("Rotten Tomatoes URL: " + "https://www.rottentomatoes.com/m/" + rotten_tomatoes_url);
 
+                consoleLog += "Title of the movie: " + JSON.parse(body).Title + "\n";
+                consoleLog += "Year the movie came out: " + JSON.parse(body).Year + "\n";
+                consoleLog += "IMDB Rating of the movie: " + JSON.parse(body).Ratings[0].Value + "\n";
+                consoleLog += "Country where the movie was produced: " + JSON.parse(body).Country + "\n";
+                consoleLog += "Language of the movie: " + JSON.parse(body).Language + "\n";
+                consoleLog += "Plot of the movie: " + JSON.parse(body).Plot + "\n";
+                consoleLog += "Actors in the movie: " + JSON.parse(body).Actors + "\n";
+                consoleLog += "Rotten Tomatoes URL: " + "https://www.rottentomatoes.com/m/" + rotten_tomatoes_url + "\n";
+
+                saveIntoLog();
             }
         });
 
     }
+
 };
 
 ////// DO-WHAT-IT-SAYS LOGIC
@@ -150,9 +206,10 @@ if(argument === "do-what-it-says") {
         //call the liri function and use the updated argument variables
         liri();
 
-
     });
 }
 else{
     liri();
+
 }
+
